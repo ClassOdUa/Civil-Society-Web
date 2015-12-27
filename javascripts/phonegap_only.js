29f6e4ad5195a8b434431e49917d33ff95239490
ClassOdUa <class.od.ua@gmail.com>
2015-12-27 04:32:44 +0200

    0.9 GPv.6

--- javascripts/phonegap_only.js
+++ javascripts/phonegap_only.js
@@ -1,6 +1,5 @@
-var isCordovaApp = !!window.cordova;
-
-if(isCordovaApp){
+var l_app = document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
+if ( l_app ) {
 	$('.btn-login-soc').hide();
 	$('.social-wrap').hide();
 	$('.refund').hide();
